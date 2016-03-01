de4ae1ae0d0fcbcdad644e0bc7f43303f5adb811
Martin Jensen <martin2610@gmail.com>
2016-03-01 10:41:22 +0100

    Added Jackson and Resource1 file.

--- src/main/java/com/rpicloud/Application.java
+++ src/main/java/com/rpicloud/Application.java
@@ -1,12 +1,38 @@
 package com.rpicloud;
 
+import com.rpicloud.models.Resource1;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping(value = "/resources")
+	public ResponseEntity<List<Resource1>> resources() {
+		List<Resource1> resources = new ArrayList<>();
+		resources.add(new Resource1("Dummy data 1"));
+		resources.add(new Resource1("Dummy data 2"));
+		resources.add(new Resource1("Dummy data 3"));
+		resources.add(new Resource1("Dummy data 4"));
+		resources.add(new Resource1("Dummy data 5"));
+
+
+		return new ResponseEntity<>(resources, HttpStatus.OK);
 	}
 }
---
+++ src/main/java/com/rpicloud/models/Resource1.java
@@ -0,0 +1,20 @@
+package com.rpicloud.models;
+
+/**
+ * Created by mixmox on 01/03/16.
+ */
+public class Resource1 {
+    String data;
+
+    public Resource1(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
